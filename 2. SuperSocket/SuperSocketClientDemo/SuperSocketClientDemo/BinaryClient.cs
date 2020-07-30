fbc065cf1d0f3ac73ed25230dc65951220571be7
Liuwei.Li <Liuwei.Li@outlook.com>
2020-07-30 23:48:07 +0800

    no msg

---
+++ 2. SuperSocket/SuperSocketClientDemo/SuperSocketClientDemo/BinaryClient.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SuperSocket.ClientEngine;
+using SuperSocket.ProtoBase;
+
+namespace SuperSocketClientDemo
+{
+    public class BinaryPackageInfo : IPackageInfo
+    {
+        public BinaryPackageInfo() { }
+
+        public BinaryPackageInfo(Int32 msgType,Int32 commandId,UInt32 errorCode,UInt32 channelId,params float[] parameters)
+        {
+            this.MsgType = msgType;
+            this.CommandId = commandId;
+            this.ErrorCode = errorCode;
+            this.ChannelId = channelId;
+            if(parameters.Length > 5)
+            {
+                throw new ArgumentException("The count of the param must not be more than 5.");
+            }
+            switch (parameters.Length)
+            {
+                case 1:
+                    Param1 = parameters[0];
+                    break;
+                case 2:
+                    Param1 = parameters[0];
+                    Param2 = parameters[1];
+                    break;
+                case 3:
+                    Param1 = parameters[0];
+                    Param2 = parameters[1];
+                    Param3 = parameters[2];
+                    break;
+                case 4:
+                    Param1 = parameters[0];
+                    Param2 = parameters[1];
+                    Param3 = parameters[2];
+                    Param4 = parameters[3];
+                    break;
+                case 5:
+                    Param1 = parameters[0];
+                    Param2 = parameters[1];
+                    Param3 = parameters[2];
+                    Param4 = parameters[3];
+                    Param5 = parameters[4];
+                    break;
+            }
+        }
+
+        public Int32 MsgType { get; set; }
+        public Int32 CommandId { get; set; }
+        public float Param1 { get; set; }
+        public float Param2 { get; set; }
+        public float Param3 { get; set; }
+        public float Param4 { get; set; }
+        public float Param5 { get; set; }
+        public UInt32 ErrorCode { get; set; }
+        public UInt32 ChannelId { get; set; }
+
+        private byte[] message;
+        public byte[] Message
+        {
+            get
+            {
+                if (message == null || message.Length != 32)
+                {
+                    message = new byte[32];
+                }
+                return message;
+            }
+            set
+            {
+                if(message == null || message.Length != 32)
+                {
+                    throw new ArgumentException("The length of the message represented by a binary array must be 32.");
+                }
+            }
+        }
+
+        public byte[] GetBytes()
+        {
+            MemoryStream ms = new MemoryStream();
+            BinaryWriter bw = new BinaryWriter(ms);
+            bw.Write(this.MsgType);
+            bw.Write(this.CommandId);
+            bw.Write(this.Param1);
+            bw.Write(this.Param2);
+            bw.Write(this.Param3);
+            bw.Write(this.Param4);
+            bw.Write(this.Param5);
+            bw.Write()
+            byte[] bytes = new byte[68];
+
+            return bytes;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+
+    public class ReqBinaryPackageInfo: BinaryPackageInfo
+    {
+        public ReqBinaryPackageInfo() : base() { }
+    }
+
+    public class RspBinaryPackageInfo : BinaryPackageInfo
+    {
+        public RspBinaryPackageInfo() : base() { }
+    }
+
+    public class BinaryFilter : FixedSizeReceiveFilter<BinaryPackageInfo>
+    {
+        public BinaryFilter(int size) : base(size) { }
+
+        public override BinaryPackageInfo ResolvePackage(IBufferStream bufferStream)
+        {
+            return new BinaryPackageInfo()
+            {
+
+            }
+        }
+    }
+
+    class BinaryClient:EasyClient<>
+    {
+        protected override void HandlePackage(IPackageInfo package)
+        {
+            base.HandlePackage(package);
+        }
+
+    }
+}
---
+++ 2. SuperSocket/SuperSocketClientDemo/SuperSocketClientDemo/Program.cs
@@ -0,0 +1,98 @@
+﻿using SuperSocket.ClientEngine;
+using SuperSocket.ProtoBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCPOUT
+{
+    /* key Param1 Param2 Param3 ... Param(N-1) ParamN\r\n
+     * 
+     */
+    internal class MyFilter : TerminatorReceiveFilter<StringPackageInfo>
+    {
+        public MyFilter():base(Encoding.ASCII.GetBytes("\r\n"))
+        {
+
+        }
+
+        public override StringPackageInfo ResolvePackage(IBufferStream bufferStream)
+        {
+            string body = bufferStream.ReadString((int)bufferStream.Length, Encoding.ASCII);
+            body = body.Replace("\r\n", "");
+            string[] entities = body.Split();
+            string key = entities[0];
+            return new StringPackageInfo(key, body, entities);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EasyClient client = new EasyClient();
+            client.Initialize(new MyFilter(), (request) =>
+            {
+                // handle the received request
+                Console.WriteLine(request.Key);
+            });
+        }
+    }
+
+    public class MyClient
+    {
+        private EasyClient client = new EasyClient();
+
+        public MyClient()
+        {
+            client.Closed += Client_Closed;
+            client.Connected += Client_Connected;
+            client.Error += Client_Error;
+        }
+
+        public void Connect()
+        {
+            Task<bool> task = client.ConnectAsync(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1222));
+            bool res = task.Result;
+            client.Initialize(new MyFilter(), new Action<StringPackageInfo>(OnReceived));
+        }
+
+        public void Send()
+        {
+            client.Send();
+        }
+
+        private void OnReceived(StringPackageInfo stringPackageInfo)
+        {
+
+        }
+
+        #region MyRegion
+        private void Client_Error(object sender, ErrorEventArgs e)
+        {
+            Console.WriteLine(e.Exception.Message);
+        }
+
+        private void Client_Connected(object sender, EventArgs e)
+        {
+            EasyClient client = (sender as EasyClient);
+            client.Send(Encoding.ASCII.GetBytes($"{client.LocalEndPoint.ToString()}成功连接服务器!"));
+        }
+
+        private void Client_Closed(object sender, EventArgs e)
+        {
+            EasyClient client = (sender as EasyClient);
+            client.Send(Encoding.ASCII.GetBytes($"{client.LocalEndPoint.ToString()}已关闭!"));
+        }
+        #endregion
+    }
+
+    public class Message
+    {
+        public string Key { get; set; }
+        public string[] Parameters { get; set; }
+    }
+}
